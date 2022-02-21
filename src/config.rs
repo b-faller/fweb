14a2d8ea247b49767896e15ab1336334fe9b7659
Benjamin Faller <git@pwned.info>
2022-02-21 21:40:33 +0100

    Refactor stuff

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 /// Information concerning the site.
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Site {
+pub struct SiteInfo {
     /// Site title.
     pub title: String,
     /// Short site description.
@@ -14,10 +14,22 @@ pub struct Site {
 /// Generation configuration and global information.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
-    pub site: Site,
-    /// Path where the built site is created at.
+    /// Information about the website
+    pub site_info: SiteInfo,
+
+    /// Directory with the website its source files.
+    /// Relative to `config.toml`.
+    #[serde(default = "default_content_path")]
+    pub content_path: PathBuf,
+
+    /// Directory where the built site is created at.
+    /// Relative to `config.toml`.
     #[serde(default = "default_output_path")]
     pub output_path: PathBuf,
+}
+
+fn default_content_path() -> PathBuf {
+    ".".into()
 }
 
 fn default_output_path() -> PathBuf {
--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,108 @@ struct Page {
     content: String,
 }
 
+#[derive(Debug)]
+pub struct Website {
+    /// Configuration for this website.
+    pub config: Config,
+}
+
+impl Website {
+    pub async fn build(&self) -> Result<()> {
+        // Read pages.
+        let pages_dir = self.config.content_path.join("pages");
+        let pages = handle_pages(pages_dir).await?;
+
+        // Read posts.
+        let posts_dir = self.config.content_path.join("posts/");
+        let posts = handle_posts(&posts_dir).await?;
+
+        // FIXME: Error handling if directory fails not because it already exists
+        tokio::fs::create_dir(&self.config.output_path).await.ok();
+
+        // Copy style.
+        let css_style = self.config.content_path.join("templates/style.css");
+        let css_style_copy = self.config.output_path.join("style.css");
+        tokio::fs::copy(&css_style, &css_style_copy)
+            .await
+            .map_err(|e| Error::Copy(css_style, css_style_copy, e))?;
+
+        // Read template
+        let html_template = self.config.content_path.join("templates/index.html");
+        let template_input = tokio::fs::read_to_string(&html_template)
+            .await
+            .map_err(|e| Error::ReadInput(html_template, e))?;
+
+        // Templating
+        let nav = pages
+            .iter()
+            .map(|p| {
+                if p.metadata.is_index {
+                    format!("<a href=\"/\">{}</a>\n", p.metadata.title)
+                } else if !p.metadata.hide {
+                    format!("<a href=\"/{}/\">{}</a>\n", p.metadata.id, p.metadata.title)
+                } else {
+                    "".to_string()
+                }
+            })
+            .collect::<String>();
+        let html = template_input.replace("%%% nav %%%", &nav);
+        let html = html.replace("%%% site_title %%%", &self.config.site_info.title);
+        let html = html.replace(
+            "%%% site_description %%%",
+            &self.config.site_info.description,
+        );
+
+        // Create articles
+        let mut html_articles = String::new();
+        for post in &posts {
+            let html = html.replace("%%% content %%%", &post.content);
+            let html_output = html.replace("%%% title %%%", &post.metadata.title);
+            html_articles += &format!(
+                "<h3><a href=\"/posts/{id}\">{title}</a></h3>\n<p>{excerpt}</p>\n<p><small>{date}</small></p>\n",
+                id=post.metadata.id, title=post.metadata.title, excerpt=post.metadata.excerpt, date=post.metadata.date.format(&DATE_FORMAT).expect("valid date")
+            );
+
+            let dir_path = self
+                .config
+                .output_path
+                .join("posts/")
+                .join(&post.metadata.id);
+            tokio::fs::create_dir_all(&dir_path)
+                .await
+                .map_err(|e| Error::CreateDirectory(dir_path.clone(), e))?;
+            let index_file_path = dir_path.join("index.html");
+            tokio::fs::write(index_file_path, html_output)
+                .await
+                .map_err(|e| Error::WriteFile(dir_path, e))?;
+        }
+
+        for page in &pages {
+            let html = html.replace("%%% content %%%", &page.content);
+            let html = html.replace("%%% articles %%%", &html_articles);
+            let html_output = html.replace("%%% title %%%", &page.metadata.title);
+
+            if page.metadata.is_index {
+                let path = self.config.output_path.join("index.html");
+                tokio::fs::write(&path, html_output)
+                    .await
+                    .map_err(|e| Error::WriteFile(path, e))?;
+            } else {
+                let dir_path = self.config.output_path.join(&page.metadata.id);
+                tokio::fs::create_dir_all(&dir_path)
+                    .await
+                    .map_err(|e| Error::CreateDirectory(dir_path.clone(), e))?;
+                let index_file_path = dir_path.join("index.html");
+                tokio::fs::write(&index_file_path, html_output)
+                    .await
+                    .map_err(|e| Error::WriteFile(index_file_path, e))?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 /// Extract frontmatter and markdown from a input file.
 fn parse_file(input: &str, filepath: impl AsRef<Path>) -> Result<(&str, &str)> {
     let mut split = input.splitn(3, "+++");
@@ -170,113 +272,38 @@ async fn handle_posts(posts_dir: impl AsRef<Path>) -> Result<Vec<Post>> {
     Ok(posts)
 }
 
-async fn build_site(source_dir: impl AsRef<Path>, cfg: &Config) -> Result<()> {
-    let source_dir = source_dir.as_ref();
-    let pages_dir = source_dir.join("pages");
-    let pages = handle_pages(pages_dir).await?;
-
-    // FIXME: Error handling if directory fails not because it already exists
-    let output_path = source_dir.join(&cfg.output_path);
-    tokio::fs::create_dir(&output_path).await.ok();
-    let style_file = source_dir.join("templates/style.css");
-    let style_file_copy = output_path.join("style.css");
-    tokio::fs::copy(&style_file, &style_file_copy)
-        .await
-        .map_err(|e| Error::Copy(style_file, style_file_copy, e))?;
-    let template_file = source_dir.join("templates/index.html");
-    let template_input = tokio::fs::read_to_string(&template_file)
-        .await
-        .map_err(|e| Error::ReadInput(template_file, e))?;
-
-    // Templating
-    let nav_html = pages
-        .iter()
-        .map(|p| {
-            if p.metadata.is_index {
-                format!("<a href=\"/\">{}</a>\n", p.metadata.title)
-            } else if !p.metadata.hide {
-                format!("<a href=\"/{}/\">{}</a>\n", p.metadata.id, p.metadata.title)
-            } else {
-                "".to_string()
-            }
-        })
-        .collect::<String>();
-    let html = template_input.replace("%%% nav %%%", &nav_html);
-    let html = html.replace("%%% site_title %%%", &cfg.site.title);
-    let html = html.replace("%%% site_description %%%", &cfg.site.description);
-
-    let posts_dir = source_dir.join("posts/");
-    let posts = handle_posts(&posts_dir).await?;
-    let mut html_articles = "".to_string();
-    for post in &posts {
-        let html = html.replace("%%% content %%%", &post.content);
-        let html_output = html.replace("%%% title %%%", &post.metadata.title);
-        html_articles += &format!(
-            "<h3><a href=\"/posts/{id}\">{title}</a></h3>\n<p>{excerpt}</p>\n<p><small>{date}</small></p>\n",
-            id=post.metadata.id, title=post.metadata.title, excerpt=post.metadata.excerpt, date=post.metadata.date.format(&DATE_FORMAT).expect("valid date")
-        );
-
-        let dir_path = output_path.join("posts/").join(&post.metadata.id);
-        tokio::fs::create_dir_all(&dir_path)
-            .await
-            .map_err(|e| Error::CreateDirectory(dir_path.clone(), e))?;
-        let index_file_path = dir_path.join("index.html");
-        tokio::fs::write(index_file_path, html_output)
-            .await
-            .map_err(|e| Error::WriteFile(dir_path, e))?;
-    }
-
-    for page in &pages {
-        let html = html.replace("%%% content %%%", &page.content);
-        let html = html.replace("%%% articles %%%", &html_articles);
-        let html_output = html.replace("%%% title %%%", &page.metadata.title);
-
-        if page.metadata.is_index {
-            let path = output_path.join("index.html");
-            tokio::fs::write(&path, html_output)
-                .await
-                .map_err(|e| Error::WriteFile(path, e))?;
-        } else {
-            let dir_path = output_path.join(&page.metadata.id);
-            tokio::fs::create_dir_all(&dir_path)
-                .await
-                .map_err(|e| Error::CreateDirectory(dir_path.clone(), e))?;
-            let index_file_path = dir_path.join("index.html");
-            tokio::fs::write(&index_file_path, html_output)
-                .await
-                .map_err(|e| Error::WriteFile(index_file_path, e))?;
-        }
-    }
-
-    Ok(())
-}
-
-async fn read_config(path: impl AsRef<Path>) -> std::io::Result<Config> {
+/// Read and parse site config
+async fn read_site_config(path: impl AsRef<Path>) -> std::io::Result<Config> {
+    // Read and parse config.
+    let path = path.as_ref();
     let content = tokio::fs::read_to_string(path).await?;
-    let config = toml::from_str(&content)?;
+    let mut config: Config = toml::from_str(&content)?;
+
+    // Make config paths relative to the configuration file.
+    let basedir = path
+        .parent()
+        .expect("file does exist and must have a parent");
+    config.content_path = basedir.join(&config.content_path);
+    config.output_path = basedir.join(&config.output_path);
+
     Ok(config)
 }
 
 async fn try_main() -> Result<()> {
-    // Get source dir from args.
-    let args: Vec<String> = std::env::args().collect();
-    let source_dir = if let Some(path) = args.get(1) {
-        let path = PathBuf::from(path);
-        path.canonicalize().map_err(|e| Error::SourceDir(path, e))?
-    } else {
-        PathBuf::from(".")
-    };
-
-    // Read website config.
-    let config_path = source_dir.join("config.toml");
-    let config = read_config(&config_path)
+    // Get website config
+    let config_path = PathBuf::from(
+        std::env::args()
+            .skip(1)
+            .next()
+            .unwrap_or("config.toml".into()),
+    );
+    let config = read_site_config(&config_path)
         .await
         .map_err(|e| Error::ConfigRead(config_path, e))?;
 
     // Build website.
-    build_site(source_dir, &config).await?;
-
-    Ok(())
+    let website = Website { config };
+    website.build().await
 }
 
 #[tokio::main]
@@ -291,5 +318,5 @@ async fn main() {
         std::process::exit(1);
     }
 
-    info!("Site built in {:?}", it.elapsed());
+    info!("Command took {:?}", it.elapsed());
 }
